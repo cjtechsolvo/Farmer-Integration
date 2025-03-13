60a5317b493005363f1a5c2cd3a4c2466149ed3d
cjtechsolvo <chirag.techsolvo@gmail.com>
2025-03-13 16:54:13 +0530

    Commiting the changes of Crop master doctype by making it dynamic with the Farm master to get the entire Farm , Site , Season and Volume data on the basis of Crop. Apart from that their are updates in Farmer Master , and Farm Master and Site master , also With Quick Entry updates in Site.

--- farmer/farmer/doctype/farm_master/farm_master.js
+++ farmer/farmer/doctype/farm_master/farm_master.js
@@ -1,8 +1,27 @@
-// Copyright (c) 2025, chirag and contributors
-// For license information, please see license.txt
+frappe.ui.form.on('Farm Master', {
+    onload: function(frm) {  
+        update_number_of_crops(frm);
+    },
+    actual_crops_add: function(frm) {  
+        update_number_of_crops(frm);
+    },
+    actual_crops_remove: function(frm) {  
+        update_number_of_crops(frm);
+    },
+    validate: function(frm) {  
+        update_number_of_crops(frm);
+    }
+});
 
-// frappe.ui.form.on("Farm Master", {
-// 	refresh(frm) {
+function update_number_of_crops(frm) {
+    let uniqueCrops = new Set();
 
-// 	},
-// });
+    (frm.doc.actual_crops || []).forEach(row => {
+        if (row.crop_name) {
+            uniqueCrops.add(row.crop_name);
+        }
+    });
+
+    frm.set_value('number_of_crops', uniqueCrops.size);
+    frm.refresh_field('number_of_crops');  // Ensures the field updates immediately
+}
